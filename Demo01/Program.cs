5f7305cf48f909066af8e3bbbc100f5722b4fc27
El Bruno <bcapuano@gmail.com>
2022-03-15 10:04:25 -0400

    1st draft of steps

---
+++ Demo01/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// create an array with weekdays
+string[] weekdays = new string[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+Console.WriteLine(weekdays);
+
+// print each day in a new line
+foreach (string day in weekdays)
+{
+    Console.WriteLine(day);
+}
