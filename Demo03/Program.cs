f7fb099f0e2b337e8e74ffed4a3205ffb4727380
El Bruno <bcapuano@gmail.com>
2022-03-15 10:17:14 -0400

    Scenario : open image and add effects

---
+++ Demo03/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using OpenCvSharp;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// open jpg image file using opencvsharp
+using (var img = new Mat(@"C:\src\reactors\GitHubCoPilotCSharp\Demo03\bruno capuano.jpg", ImreadModes.Color))
+{
+    // show image in the output window
+    Cv2.ImShow("image", img);
+    Cv2.WaitKey(0);
+}
