b9c0f838995d80d59f94cb16f4fb580c32d9af28
El Bruno <bcapuano@gmail.com>
2022-03-15 10:09:15 -0400

    Scenario: Open and process data in large files

---
+++ Demo02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// open csv file and show each line in the output window
+using (var reader = new StreamReader(@"C:\src\reactors\GitHubCoPilotCSharp\Demo02\book2.csv"))
+{
+    while (!reader.EndOfStream)
+    {
+        var line = reader.ReadLine();
+        Console.WriteLine(line);
+    }
+}
